d7a02479d673c40e6cbae2b1b8e877d48ee9828a
sebi <gore.sebyx@yahoo.com>
2021-02-06 20:17:12 +0000

    change

--- .build/thor/repo.rb
+++ .build/thor/repo.rb
@@ -3,7 +3,11 @@ class Repo < Thor
 
   desc "clone", "clones personal-social-media repo"
   def clone
-    run "git clone https://github.com/personal-social-media/personal-social-media.git app --depth 1"
+    inside ".build/releases" do
+      run "git clone https://github.com/personal-social-media/personal-social-media.git #{release_id} --depth 1"
+    end
+
+    run "ln -s .build/releases app"
   end
 
   desc "update", "updates the app"
@@ -11,6 +15,12 @@ class Repo < Thor
     inside "./app" do
       run "git checkout Gemfile.lock"
       run "git pull origin master"
+    end
+  end
+
+  no_commands do
+    def release_id
+      @release_id ||= Time.now.to_i.to_s
     end
   end
 end
