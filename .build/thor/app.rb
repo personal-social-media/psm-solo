7d346dcb2c5b985440f36f2928f31276a836743e
sebi <gore.sebyx@yahoo.com>
2021-03-08 08:14:10 +0000

    update

--- .build/thor/app.rb
+++ .build/thor/app.rb
@@ -3,7 +3,7 @@ class App < Thor
 
   desc "deps", "install ruby gems and node modules"
   def deps
-    run "docker-compose run app bundle install --jobs 4 --without development test"
+    run "docker-compose run app gem install bundler && bundle install --jobs 4 --without development test"
     run "docker-compose run app bundle clean --force"
   end
 
